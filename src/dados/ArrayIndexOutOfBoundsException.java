4c02110bc7fe03e286dc8a7af7503c0deab2cc98
Tayse Alves <86623444+Tayse-Alvess@users.noreply.github.com>
2021-07-09 14:29:48 -0300

    Criação de tratamento de exceção
    Adição do tratamento de exceção para erro ao inserir Pessoa fora da quantidade permitida

---
+++ src/dados/ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,8 @@
+package dados;
+
+public class ArrayIndexOutOfBoundsException extends Exception {
+	
+  public ArrayIndexOutOfBoundsException() {
+	  super("Erro! Limite de tamanho atingido! Pessoa não cadastrada no Array");
+	}
+}
