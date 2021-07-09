af11265aef7f5cf9955d7b7332827db5217eaff3
Tayse Alves <86623444+Tayse-Alvess@users.noreply.github.com>
2021-07-09 18:16:55 -0300

    Remoção de bugs

--- src/TestaPessoa.java
+++ src/TestaPessoa.java
@@ -8,6 +8,7 @@ import negocios.Pessoa;
 import negocios.Professor;
 import negocios.RepositorioPessoas;
 import dados.PessoaDAO;
+import dados.ArrayIndexOutOfBoundsException;
 
 public class TestaPessoa {
 
@@ -18,9 +19,9 @@ public class TestaPessoa {
 		// Repositorios e Banco de Dados
 		RepositorioPessoasArray repArray = new RepositorioPessoasArray(2);
 		RepositorioPessoas repLista = new RepositorioPessoasLista(); //Polimorfismo
-		PessoaDAO banco = new Pessoa DAO();
+		PessoaDAO banco = new PessoaDAO();
 
-		// Criação de objetos
+		// Criacao de objetos
 		Curso curso = new Curso();
 
 		// Inicio - digitar letra por letra
@@ -36,7 +37,7 @@ public class TestaPessoa {
 		boolean on = true;
 		int escolha;
 
-		// Loop do menu, até escolher a opção sair
+		// Loop do menu, ate escolher a opcao sair
 		while (on == true) {
 			// Menu
 			System.out.println("-------- Menu -------- " + "\n");
@@ -134,7 +135,7 @@ public class TestaPessoa {
 					// Salvar o cadastro do Professor no Array, na Lista e no Banco de Dados
 					Professor professor = new Professor(nome, cpf, idade, salario);
 					
-					/*Verifica se está no limite da quantidade de pessoas que podem ser
+					/*Verifica se esta no limite da quantidade de pessoas que podem ser
 					  armazenadas no Array*/
 					try{
 						repArray.inserir(professor);
@@ -157,8 +158,8 @@ public class TestaPessoa {
 				
 				//Escolha entre procurar aluno ou professor
 				System.out.println("1. Aluno" + "\n" + "2. Professor");
-				System.out.print("Escolha uma opção: ");
-				escolha = input.next();
+				System.out.print("Escolha uma opcao: ");
+				escolha = input.nextInt();
 				
 				// Caso a pessoa digite algo diferente de 1 ou 2, é necessário escolher
 				// novamente
@@ -177,9 +178,9 @@ public class TestaPessoa {
 				 */
 				Pessoa procurado = repLista.procurar(input.next());
 
-				// Se retornar null, então não existe. Caso exista, utilizei toString para o print
+				// Se retornar null, então nao existe. Caso exista, utilizei toString para o print
 				if (procurado == null)
-					System.out.println("\n" + "\n" + "Não há pessoa cadastrada com esse CPF!");
+					System.out.println("\n" + "\n" + "Nao ha pessoa cadastrada com esse CPF!");
 				else
 					System.out.println("\n" + "\n" + "Pessoa encontrada!" + "\n" + procurado.toString());
 
@@ -191,8 +192,8 @@ public class TestaPessoa {
 					
 				//Escolha entre remover aluno ou professor
 				System.out.println("1. Aluno" + "\n" + "2. Professor");
-				System.out.print("Escolha uma opção: ");
-				escolha = input.next();
+				System.out.print("Escolha uma opcao: ");
+				escolha = input.nextInt();
 				
 				// Caso a pessoa digite algo diferente de 1 ou 2, é necessário escolher
 				// novamente
@@ -223,7 +224,7 @@ public class TestaPessoa {
 					else
 						banco.removeProfessor(auxiliar);
 					
-					//Verifica se está no Array - É necessario porque Array está com tamanho 2
+					//Verifica se esta no Array - e necessario porque Array esta com tamanho 2
 					procurado = repArray.procurar(auxiliar);
 					//Remove no Array
 					if(procurado != null)
@@ -242,7 +243,7 @@ public class TestaPessoa {
 				System.out.println("3. Todos");
 				System.out.print("Escolha uma opção: ");
 				escolha = input.nextInt();
-				// Caso a pessoa digite algo diferente de 1 ou 2 ou 3, é necessário escolher
+				// Caso a pessoa digite algo diferente de 1 ou 2 ou 3, e necessario escolher
 				// novamente
 				while (escolha != 1 && escolha != 2 && escolha !=3) {
 					System.out.println("\n" + "Escolha invalida!");
@@ -270,8 +271,8 @@ public class TestaPessoa {
 				//Listagem de todas as pessoas - Utilizando Lista
 				else{
 					/*
-				 	* Quando se utiliza o método listarPessoas, ele retorna uma Lista com todas as
-				 	* pessoas Mas a formatação fica separado por "," e "]" Então utilizei um for,
+				 	* Quando se utiliza o metodo listarPessoas, ele retorna uma Lista com todas as
+				 	* pessoas Mas a formatacao fica separado por "," e "]" Entao utilizei um for,
 				 	* para percorrer esta lista e ir printando cada pessoa de cada vez
 				 	* (RepositorioPessoaLista) repLista).listarPessoas().size() --> Isso me retorna
 				 	* o tamanho da lista. Em outras palavras, me retorna a quantidade de pessoas
@@ -285,13 +286,13 @@ public class TestaPessoa {
 
 			// Sair
 			case 5:
-				// Atualiza a variavel e a aplicação não mais volta para o Menu
+				// Atualiza a variavel e a aplicacao não mais volta para o Menu
 				on = false;
 				break;
 
-			// Opção inválida - Qualquer outro numero digitado no menu será invalido
+			// Opção inválida - Qualquer outro numero digitado no menu sera invalido
 			default:
-				System.out.println("Opcao inválida! Por favor, escolha um número válido!");
+				System.out.println("Opcao invalida! Por favor, escolha um numero valido!");
 				break;
 			}
 
